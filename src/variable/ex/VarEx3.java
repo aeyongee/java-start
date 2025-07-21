044bacca7ec356a90aa56f93858f1615f54fa5b5
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-07-21 22:56:30 +0900

    섹션3. 변수 마무리

---
+++ src/variable/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long varLong = 10000000000L;
+        System.out.println(varLong);
+        boolean varBoolean = true;
+        System.out.println(varBoolean);
+    }
+}
