edc2eced11f626b8fc9bae16edd7208399d40872
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-09-06 18:39:49 +0900

    섹션 6. 반복문 마무리

---
+++ src/loop/ex/ForEx1.java
@@ -0,0 +1,15 @@
+package loop.ex;
+
+public class ForEx1 {
+    public static void main(String[] args) {
+        int count = 1;
+        for (count = 1; count < 11; count++) {
+            System.out.println(count);
+        }
+
+//        정답 풀이
+//        for (int count = 1; count <= 1-; count++) {
+//            System.out.println(count);
+//        }
+    }
+}
