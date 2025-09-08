4b6202a8aa87d471e6e009e9dc2e4c3aa1cd6ce4
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-09-08 23:54:56 +0900

    섹션 8. 훈련 마무리

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,34 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0, count = 0, num = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요: ");
+
+//        while (true) {
+//            int num = input.nextInt();
+//
+//            if (num == -1) {
+//                break;
+//            }
+//
+//            sum += num;
+//            count++;
+//
+//        }
+
+        while((num = input.nextInt()) != -1) {
+            sum += num;
+            count++;
+        }
+
+        double average = (double) sum / count;
+
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + average);
+    }
+}
